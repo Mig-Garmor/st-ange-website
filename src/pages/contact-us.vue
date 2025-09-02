5726cc752250ee2c940c1952990e62dad3ba8543
Mig-Garmor <thecodermiguel@gmail.com>
2025-09-02 21:35:02 +0200

    Contact us page created

---
+++ src/pages/contact-us.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PageLayout from "../components//wrappers/PageLayout.vue";
+</script>
+
+<template>
+  <PageLayout>
+    <div>Contact Us</div>
+  </PageLayout>
+</template>
+
+<style src="@styles/pages/contact-us.css"></style>
