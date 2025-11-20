b9b52c7e4521c14c9998b7da759dcf758bd27875
Mig-Garmor <thecodermiguel@gmail.com>
2025-11-20 20:19:11 +0100

    Strategies to ensure meta description is loaded

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ import router from "./router";
 
 import App from "./App.vue";
 
+if (!document.querySelector('meta[name="description"]')) {
+  const meta = document.createElement("meta");
+  meta.name = "description";
+  meta.content =
+    "St Ange Auto Service is a trusted auto repair shop in Baytown, Texas. We provide expert car alignment, A/C repair, brakes, tires, and more. Call 832-572-7121 for quality mechanic service today!";
+  document.head.appendChild(meta);
+}
+
 createApp(App).use(router).mount("#app");
