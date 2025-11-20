9e7c923ac1a90ff59951d7a38c01a15ccb0fd509
Mig-Garmor <thecodermiguel@gmail.com>
2025-11-20 20:14:06 +0100

    Google maps widget title for accessibility

--- src/components/blocks/GoogleMapsWidget.vue
+++ src/components/blocks/GoogleMapsWidget.vue
@@ -5,7 +5,7 @@ const { isMobile } = useDeviceType();
 </script>
 
 <template>
-  <div class="google-maps-widget">
+  <div class="google-maps-widget" title="Google maps widget">
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.2548993612645!2d-94.9661879244063!3d29.74133527507762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x863f5d96d78f1689%3A0x9d76a9d056bdbd40!2sSt%20Ange%20Auto%20Service!5e0!3m2!1sen!2smt!4v1758451884017!5m2!1sen!2smt"
       :width="isMobile ? '350' : '600'"
